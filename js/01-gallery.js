f4c0ef581d517f7fe1a5276da813e9ed83280e75
Iryna Hudymenko <iryn4ik09@gmail.com>
2021-12-16 00:50:21 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const palleteContainer = document.querySelector('.gallery');
+const cardsMarkup = createColorCardMarkup();
+
+function createColorCardMarkup() {
+
+    return galleryItems.map(item =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src='${item.preview}'
+      data-source='${item.original}'
+      alt='${item.description}'
+    />
+  </a>
+</div>`).join('');
+}
+palleteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+palleteContainer.addEventListener('click', onImageToCreateModal);
+function onImageToCreateModal(event) {
+    event.preventDefault();
+
+    const selectedImage = event.target.dataset.source;
+    
+    const modalCreate = basicLightbox.create(`
+    <img src="${selectedImage}" width = "800" heigth = "600">`);
+    modalCreate.show();
+    
+       }
+
+palleteContainer.addEventListener('click', onPalleteContainerClick);
+   
+function onPalleteContainerClick(event) {
+    if (event.target.nodeName !== "IMG") { return; };
+    console.log(event.target.nodeName);
+      
+}
