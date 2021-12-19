290c9f652d38ed17f8574ebafa630a9cafc020df
Iryna Hudymenko <iryn4ik09@gmail.com>
2021-12-19 14:34:26 +0200

    Доработка галерея со стрелочками

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,16 +2,17 @@ import { galleryItems } from './gallery-items.js';
 
 const palleteContainer = document.querySelector('.gallery');
 const cardsMarkup = createColorCardMarkup();
-
+let currentIndex = 0;
 function createColorCardMarkup() {
 
-    return galleryItems.map(item =>
+    return galleryItems.map((item, index) =>
         `<div class="gallery__item">
   <a class="gallery__link" href="${item.original}">
     <img
       class="gallery__image"
       src='${item.preview}'
       data-source='${item.original}'
+      data-index="${index}"
       alt='${item.description}'
     />
   </a>
@@ -21,14 +22,19 @@ palleteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
 palleteContainer.addEventListener('click', onImageToCreateModal);
 
-const instance = basicLightbox.create
-  (`<img class = 'modal-ing' src="">`, {
-    onShow: instance => {
-  window.addEventListener('keydown', onEscClick);
+const options = {
+  onShow: () => { console.log("open");
+  window.addEventListener('keydown', pressKey);
 },
-    onClose: instance => {
-  window.removeEventListener('keydown', onEscClick);
-},});
+  onClose: () => {
+    console.log("close");
+    window.removeEventListener('keydown', pressKey);
+  }
+
+}
+
+const instance = basicLightbox.create
+  (`<img class = 'modal-ing' src="">`, options);
 
 function onImageToCreateModal(event) {
   event.preventDefault();
@@ -36,15 +42,69 @@ function onImageToCreateModal(event) {
   if (event.target.nodeName !== "IMG") { return; };
   console.log(event.target.nodeName);
 
-  instance.element().querySelector('img').src = event.target.dataset.source;
+currentIndex = Number(event.target.dataset.index);
+  setImg(event.target.dataset.source);
   instance.show();
 }
-function onEscClick(evt) {
-  if (evt.code === 'Escape') {
-    instance.close();
-    return;
+function setImg(url) {
+   instance.element().querySelector('img').src = url
+}
+function pressKey({ code }) {
+  switch (code) {
+    case 'Escape':
+      instance.close();
+      break;
+    case 'ArrowRight':
+      currentIndex += 1;
+      if (currentIndex >= galleryItems.length) {
+        currentIndex = 0;
+      }
+      console.log(currentIndex);
+      setImg(galleryItems[currentIndex].original);
+      break;
+    case 'ArrowLeft':
+      currentIndex -= 1;
+      if (currentIndex < 0) {
+        currentIndex = galleryItems.length - 1;
+      }
+     setImg(galleryItems[currentIndex].original);
+      break;
+    default:
+      console.log(Error);
+      break;
   }
 }
+  
+
+
+
+
+
+
+
+
+// function onEscClick(evt) {
+//   if (evt.code === 'Escape') {
+//     instance.close();
+//     return;
+//   }
+//   if (evt.code === "ArrowRight") {
+//     currentIndex += 1;
+//      if (currentIndex >= galleryItems.length) {
+//       currentIndex = 0;
+//     }
+//     console.log(currentIndex);
+//     instance.element().querySelector('img').src = galleryItems[currentIndex].original;
+//       }
+//   if (evt.code === "ArrowLeft") {
+//     currentIndex -= 1;
+//     if (currentIndex < 0) {
+//       currentIndex = galleryItems.length - 1;
+//     }
+//     console.log(currentIndex);
+//      instance.element().querySelector('img').src = galleryItems[currentIndex].original;
+//   }
+// }
   
 
 //     const selectedImage = event.target.dataset.source;
